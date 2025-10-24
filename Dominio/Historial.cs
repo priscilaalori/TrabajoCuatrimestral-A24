8653ad58df4f08809240ac3950a1c18cf95cd332
PameKid <pamelat.vizcarra@gmail.com>
2025-10-24 14:31:45 -0300

    Modelo de dominio modificaciones sugeridas

---
+++ Dominio/Historial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Historial
+    {
+
+        public int IdHistorial { get; set; }
+        public int IdDeportista { get; set; }
+        public List<Observacion> Observaciones{ get; set; }
+        public List<Rutina> Rutinas { get; set; }
+
+    }
+}
---
+++ Dominio/Observacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Observacion
+    {
+            public int IdObservacion { get; set; }
+            public int IdRutina { get; set; }
+            public int IdProfesor { get; set; } //
+            public string Comentario { get; set; }
+            public DateTime Fecha { get; set; }
+      
+    }
+}
