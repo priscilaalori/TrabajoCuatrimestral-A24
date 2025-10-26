3f56cace094fe8c6d466034df5c97b41cda10f0b
PameKid <pamelat.vizcarra@gmail.com>
2025-10-25 21:51:19 -0300

    Ventana Vista y eliminación de Agenda

--- Dominio/Reserva.cs
+++ Dominio/Reserva.cs
@@ -8,12 +8,19 @@ namespace Dominio
 {
     public class Reserva
     {
-        public int IdAgenda { get; set; }
+        public int IdReserva { get; set; }
         public int IdProfesor { get; set; }
+
+        public int IdDeportista { get; set; }
         public DateTime Fecha { get; set; }
         public DateTime HoraInicio { get; set; }
         public DateTime HoraFin { get; set; }
         public bool Disponible { get; set; }
+        public string TipoEntrenamiento { get; set; } // ej: Técnica de nado, Fuerza, Resistencia
+        public string Descripcion { get; set; }
+        public string Estado { get; set; } // Pendiente, Aceptada, Rechazada, Realizada, Cancelada
+        public DateTime FechaCreacion { get; set; }
+        public string Ubicacion { get; set; } // opcional: lugar físico o virtual
 
     }
 }
---
+++ TrabajoCuatrimestral-A24/vistaEntrenadorDeportista.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="vistaEntrenadorDeportista.aspx.cs" Inherits="tp_webform_equipo_24A.vistaEntrenadorDeportista" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <h1>Mis rutinas </h1>
+
+
+</asp:Content>
